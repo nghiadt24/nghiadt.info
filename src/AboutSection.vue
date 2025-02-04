59cf03d8b873a84f109dfa1ae214087384c45ff5
nghiadt24 <nghiadinh2244@gmail.com>
2025-02-04 21:45:01 +0700

    update about section

--- src/AboutSection.vue
+++ src/AboutSection.vue
@@ -29,8 +29,55 @@ import './scss/components.scss'
     <div class="h-divider"></div>
     <div class="AboutServices">
       <h2 class="AboutTitle">Services</h2>
-      <div class="">
-
+      <div class="AboutCardWrap row">
+        <div class="AboutCard col-4">
+          <div class="AboutCardIcon">
+            <PhBrowser :size="28" />
+          </div>
+          <div class="AboutCardText">
+            <h3 class="AboutCardTitle">
+              Web Design
+            </h3>
+            <p class="AboutCardDes">
+              Crafting stunning and responsive websites that capture your brand’s essence and engage your audience.</p>
+          </div>
+        </div>
+        <div class="AboutCard col-4">
+          <div class="AboutCardIcon">
+            <PhBoundingBox :size="28" />
+          </div>
+          <div class="AboutCardText">
+            <h3 class="AboutCardTitle">
+              UI/UX Design
+            </h3>
+            <p class="AboutCardDes">
+              Enhancing UX with intuitive and aesthetically pleasing interface designs for both web and mobile applications.</p>
+          </div>
+        </div>
+        <div class="AboutCard col-4">
+          <div class="AboutCardIcon">
+            <PhBezierCurve :size="28" />
+          </div>
+          <div class="AboutCardText">
+            <h3 class="AboutCardTitle">
+              Brand Identity
+            </h3>
+            <p class="AboutCardDes">
+              Creating cohesive brand strategies, including logo design, color schemes, typography, and brand guidelines.</p>
+          </div>
+        </div>
+        <div class="AboutCard col-4">
+          <div class="AboutCardIcon">
+            <PhBezierCurve :size="28" />
+          </div>
+          <div class="AboutCardText">
+            <h3 class="AboutCardTitle">
+              Brand Identity
+            </h3>
+            <p class="AboutCardDes">
+              Creating cohesive brand strategies, including logo design, color schemes, typography, and brand guidelines.</p>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -43,6 +90,7 @@ import './scss/components.scss'
 .AboutSection {
   height: 100vh;
   display: flex;
+  padding: 84px 0px;
   flex-direction: column;
   justify-content: center;
   align-items: start;
@@ -98,5 +146,61 @@ import './scss/components.scss'
   height: 1px;
   width: 100%;
   background-color: #d9d9d9;
+}
+.AboutServices {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  gap: 32px;
+}
+.AboutCardWrap {
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+  gap: 24px;
+}
+.AboutCard {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: start;
+  border-radius: 16px;
+  height: 284px;
+  @include color-border-neutral-medium;
+  padding: 24px;
+  width: 100%;
+}
+.AboutCardIcon {
+  @include color-bg-neutral-strong;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  padding: 8px;
+  @include color-content-white;
+}
+.AboutCardText {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: start;
+  gap: 8                                                 q  px;
+}
+.AboutCardTitle {
+  @include color-content-neutral-strong;
+  text-align: right;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 36px;
+}
+.AboutCardDes {
+  @include color-content-neutral-medium;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: 26px;
 }
 </style>
